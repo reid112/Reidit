d9fb6973764ca10583ac9a9f962728f8b62bd95f
reid112 <rreid112@gmail.com>
2017-07-06 23:48:39 -0600

    Playing with Butterknife to make sure I have things working

--- app/src/main/java/ca/rjreid/reidit/ui/main/MainActivity.kt
+++ app/src/main/java/ca/rjreid/reidit/ui/main/MainActivity.kt
@@ -1,15 +1,33 @@
 package ca.rjreid.reidit.ui.main
 
 import android.os.Bundle
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
 import ca.rjreid.reidit.R
 import ca.rjreid.reidit.ui.base.BaseActivity
 
 class MainActivity : BaseActivity() {
 
+    private var counter = 0
+
+    @BindView(R.id.test_textView) lateinit var testTextView: TextView
+
     //region Lifecycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ButterKnife.bind(this)
     }
+    //endregion
+
+    //region Click Listeners
+    @OnClick(R.id.test_button)
+    internal fun testButtonClicked() {
+        counter++
+        testTextView.text = counter.toString()
+    }
+
     //endregion
 
     //region BaseActivity Implementation
